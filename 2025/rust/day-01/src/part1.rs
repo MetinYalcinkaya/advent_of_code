6bb5faa5790057ecf289732e88b228d89cfe1372
Metin Yalcinkaya <metinyalcinkaya@hotmail.com.au>
2025-12-02 16:43:16 +1100

    Revert "added bench txt to gitignore"
    This reverts commit 85759f287c808a8b929f6241b25c12d8631194fa.

--- 2025/rust/day-01/src/part1.rs
+++ 2025/rust/day-01/src/part1.rs
@@ -1,49 +1,6 @@
 #[tracing::instrument]
-pub fn process(input: &str) -> miette::Result<String> {
-    let dirs: Vec<(&str, i32)> = input
-        .lines()
-        .map(|s| {
-            let iter = s.split_at(1);
-            let (first, second) = (iter.0, iter.1.parse::<i32>().unwrap());
-            (first, second)
-        })
-        .collect();
-    let mut dial = Dial { current: 50 };
-    let mut zero_count: i32 = 0;
-    dbg!(&dirs);
-
-    for (dir, num) in dirs {
-        if dir == "L" {
-            dial.turn_left(num);
-        } else {
-            dial.turn_right(num);
-        }
-
-        if dial.current == 0 {
-            zero_count += 1;
-        }
-    }
-
-    Ok(zero_count.to_string())
-}
-
-// dial has bounds: (0, 99)
-struct Dial {
-    current: i32,
-}
-
-impl Dial {
-    fn turn_left(&mut self, num: i32) {
-        let mut dial = self.current;
-        dial = (dial - num).rem_euclid(100);
-        self.current = dial;
-    }
-
-    fn turn_right(&mut self, num: i32) {
-        let mut dial = self.current;
-        dial = (dial + num).rem_euclid(100);
-        self.current = dial;
-    }
+pub fn process(_input: &str) -> miette::Result<String> {
+    todo!("day 01 - part 1");
 }
 
 #[cfg(test)]
@@ -52,17 +9,9 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        let input = "L68
-L30
-R48
-L5
-R60
-L55
-L1
-L99
-R14
-L82";
-        assert_eq!("3", process(input)?);
+        todo!("haven't done test yet");
+        let input = "";
+        assert_eq!("", process(input)?);
         Ok(())
     }
 }
